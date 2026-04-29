b6dab3bc188b0f2b2394ea021bdccb77d22b8aa5
Tiavina Anjara <anjara.tiavina10@gmail.com>
2026-04-29 12:44:05 +0300

    vaovao

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -49,12 +49,12 @@
         Utilisateurs
         <span class="nav-badge">24</span>
       </a>
-      <a href="form.html" class="nav-item">
+      <a href="etudiant" class="nav-item">
         <svg viewBox="0 0 24 24">
           <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
           <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
         </svg>
-        Formulaire
+        Etudiants
       </a>
 
       <div class="sidebar-section">Modules</div>
